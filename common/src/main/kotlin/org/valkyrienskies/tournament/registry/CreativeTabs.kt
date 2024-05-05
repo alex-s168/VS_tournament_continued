1b50cf665b59f85fc25d46e1ec40460f9b8b4c13
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-05-05 11:21:52 +0200

    forge and don't show old items and old blocks in tabs

--- common/src/main/kotlin/org/valkyrienskies/tournament/registry/CreativeTabs.kt
+++ common/src/main/kotlin/org/valkyrienskies/tournament/registry/CreativeTabs.kt
@@ -1,10 +1,14 @@
 package org.valkyrienskies.tournament.registry
 
 import net.minecraft.network.chat.Component
+import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.ItemStack
 import org.valkyrienskies.tournament.TournamentBlocks
 import org.valkyrienskies.tournament.TournamentItems
+import org.valkyrienskies.tournament.blocks.OldBlock
+import org.valkyrienskies.tournament.items.old.OldItem
+import org.valkyrienskies.tournament.items.old.UpdateItem
 
 object CreativeTabs {
     fun create(): CreativeModeTab {
@@ -12,9 +16,11 @@ object CreativeTabs {
             .title(Component.translatable("itemGroup.vs_tournament.main_tab"))
             .icon { ItemStack(TournamentBlocks.SHIP_ASSEMBLER.get().asItem()) }
             .displayItems { _, output ->
-                TournamentItems.ITEMS.forEach {
-                    output.accept(it.get())
-                }
+                TournamentItems.ITEMS
+                    .map { it.get() }
+                    .filter { it !is OldItem && it !is UpdateItem }
+                    .filter { if (it is BlockItem) it.block !is OldBlock else true }
+                    .forEach(output::accept)
             }
             .build()
     }
--- forge/src/main/kotlin/org/valkyrienskies/tournament/forge/TournamentModForge.kt
+++ forge/src/main/kotlin/org/valkyrienskies/tournament/forge/TournamentModForge.kt
@@ -1,7 +1,8 @@
 package org.valkyrienskies.tournament.forge
 
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
-import net.minecraft.core.registries.Registries
+import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraftforge.client.event.EntityRenderersEvent.RegisterRenderers
@@ -10,17 +11,22 @@ import net.minecraftforge.event.TickEvent.ServerTickEvent
 import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
 import org.valkyrienskies.tournament.TickScheduler
+import org.valkyrienskies.tournament.TournamentItems.TAB
 import org.valkyrienskies.tournament.TournamentMod
 import org.valkyrienskies.tournament.TournamentMod.init
 import org.valkyrienskies.tournament.TournamentMod.initClient
 import org.valkyrienskies.tournament.TournamentMod.initClientRenderers
 import org.valkyrienskies.tournament.TournamentModels
+import org.valkyrienskies.tournament.registry.CreativeTabs.create
 import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
+
 @Mod(TournamentMod.MOD_ID)
 class TournamentModForge {
+
     private var happendClientSetup = false
 
     init {
@@ -28,7 +34,14 @@ class TournamentModForge {
             TickScheduler.tickServer(event.server)
         }
 
-        // Submit our event bus to let architectury register our content on the right time
+        MOD_BUS.addListener { event: FMLCommonSetupEvent ->
+            Registry.register(
+                BuiltInRegistries.CREATIVE_MODE_TAB,
+                TAB,
+                create()
+            )
+        }
+
         MOD_BUS.addListener { event: FMLClientSetupEvent? ->
             clientSetup(
                 event
