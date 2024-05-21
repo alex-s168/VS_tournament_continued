2bb0337afd9d80c9b386d0ea8db414969a551102
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-05-21 20:32:43 +0200

    fixed sensor beeing offset

--- common/src/main/kotlin/org/valkyrienskies/tournament/blockentity/RopeHookBlockEntity.kt
+++ common/src/main/kotlin/org/valkyrienskies/tournament/blockentity/RopeHookBlockEntity.kt
@@ -34,8 +34,8 @@ class RopeHookBlockEntity(pos: BlockPos, state: BlockState):
     fun setRopeID(rope: ConstraintId, mainIn: Vector3d?, otherIn: Vector3d?, level: Level) {
         println("Block>> $rope")
 
-        val main = Helper3d.convertShipToWorldSpace(level, mainIn!!).add(Vector3d(0.5, 0.5, 0.5))
-        val other = Helper3d.convertShipToWorldSpace(level, otherIn!!).add(Vector3d(0.5, 0.5, 0.5))
+        val main = Helper3d.convertShipToWorldSpace(level, mainIn!!.add(0.5, 0.5, 0.5, Vector3d()))
+        val other = Helper3d.convertShipToWorldSpace(level, otherIn!!.add(0.5, 0.5, 0.5, Vector3d()))
 
         ropeId = rope
         otherPos = other
